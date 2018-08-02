2bf53861562340a5789e363a3fc3771e58886c56
Alonso Zhang <code@zhangwu.tech>
2018-08-02 18:26:21 +0800

    Initial commit

---
+++ LeetCode/ListNode.swift
@@ -0,0 +1,18 @@
+//
+//  ListNode.swift
+//  LeetCode
+//
+//  Created by Alonso on 2018/8/2.
+//  Copyright © 2018 Alonso. All rights reserved.
+//
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
